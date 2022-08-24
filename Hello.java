3c5b4a01830c47120a85ce10e07a77dd10b0d035
corralba <112033527+corralba@users.noreply.github.com>
2022-08-24 19:20:06 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/********************************************************************************************* Hello.java
+********************************************************************************************** Britney Corral
+* 2022-24-08
+* CMCS255 Section004
+********************************************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+	
+	System.out.println("Britney");
+	System.out.println("Hello.java");
+	System.out.println("This program will....");
+	System.out.println("2022-24-08");
+	System.out.println("CMCS255 Section004");
+	System.out.println();
+
+	}
+}	
+
